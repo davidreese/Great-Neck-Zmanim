38d93e821602e0207cf732bd681307c855d9f6ca
David Reese <david@reesedevelopment.com>
2022-01-12 06:23:43 +0200

    moved navbar into seperate file and imported the date

--- src/main/java/com/reesedevelopment/greatneckzmanim/GreatNeckZmanimApplication.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/GreatNeckZmanimApplication.java
@@ -3,11 +3,12 @@ package com.reesedevelopment.greatneckzmanim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class GreatNeckZmanimApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GreatNeckZmanimApplication.class, args);
     }
-
 }
