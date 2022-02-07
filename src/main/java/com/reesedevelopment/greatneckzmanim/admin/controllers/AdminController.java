09445ef78a8f5adfd18a6eee4d3c259cf1298198
David Reese <david@reesedevelopment.com>
2022-02-07 12:16:02 +0200

    added logout function

--- src/main/java/com/reesedevelopment/greatneckzmanim/admin/controllers/AdminController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/admin/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package com.reesedevelopment.greatneckzmanim.admin.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,13 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
-    public ModelAndView admin() {
-        return login();
+    public ModelAndView admin(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) boolean logout) {
+        return new LoginController().login(error, logout);
+    }
+
+    @GetMapping("/admin/logout")
+    public ModelAndView logout(@RequestParam(value = "error", required = false) String error) {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return new LoginController().login(error, true);
     }
 }
