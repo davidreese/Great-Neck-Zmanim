367d423d8eb9b5dcef4fd8055d1095ffebf04167
David Reese <david@reesedevelopment.com>
2022-04-01 08:58:22 +0300

    removed old files

--- src/main/java/com/reesedevelopment/greatneckzmanim/admin/users/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reesedevelopment.greatneckzmanim.admin.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<AppUser, Integer> {
-    AppUser findByUsername(String username);
-}
