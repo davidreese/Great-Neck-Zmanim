bbdf741909ed82be0262476e45e2e8c0971f1080
David Reese <david@reesedevelopment.com>
2022-04-27 11:12:17 -0400

    Schedule added

---
+++ src/main/java/com/reesedevelopment/greatneckzmanim/admin/structure/minyanim/Schedule.java
@@ -0,0 +1,61 @@
+package com.reesedevelopment.greatneckzmanim.admin.structure.minyanim;
+
+public class Schedule {
+    private Time sunday;
+    private Time monday;
+    private Time tuesday;
+    private Time wednesday;
+    private Time thursday;
+    private Time friday;
+    private Time shabbat;
+    private Time roshChodesh;
+    private Time yomTov;
+
+    public Schedule(Time sunday, Time monday, Time tuesday, Time wednesday, Time thursday, Time friday, Time shabbat, Time roshChodesh, Time yomTov) {
+        this.sunday = sunday;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.shabbat = shabbat;
+        this.roshChodesh = roshChodesh;
+        this.yomTov = yomTov;
+    }
+
+    public Time getSunday() {
+        return sunday;
+    }
+
+    public Time getMonday() {
+        return monday;
+    }
+
+    public Time getTuesday() {
+        return tuesday;
+    }
+
+    public Time getWednesday() {
+        return wednesday;
+    }
+
+    public Time getThursday() {
+        return thursday;
+    }
+
+    public Time getFriday() {
+        return friday;
+    }
+
+    public Time getShabbat() {
+        return shabbat;
+    }
+
+    public Time getRoshChodesh() {
+        return roshChodesh;
+    }
+
+    public Time getYomTov() {
+        return yomTov;
+    }
+}
