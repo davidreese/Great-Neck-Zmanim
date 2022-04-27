c2b1022769a9dd4f575ea6c5049630be4bb5fc86
David Reese <david@reesedevelopment.com>
2022-04-27 09:14:47 -0400

    fixed error reporting on new-organization.html, closes #6

--- src/main/java/com/reesedevelopment/greatneckzmanim/admin/controllers/AdminController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/admin/controllers/AdminController.java
@@ -115,8 +115,7 @@ public class AdminController {
     }
 
     @RequestMapping(value = "/admin/new-organization", method = RequestMethod.GET)
-    public ModelAndView addOrganization(@RequestParam(value = "success", required = false) boolean success,
-                                        @RequestParam(value = "error", required = false) String error) {
+    public ModelAndView addOrganization(boolean success, String error, String inputErrorMessage) {
         if (!isSuperAdmin()) {
             throw new AccessDeniedException("You are not authorized to access this page.");
         }
@@ -126,8 +125,9 @@ public class AdminController {
         Date today = new Date();
         mv.getModel().put("date", dateFormat.format(today));
 
-        mv.getModel().put("success", success);
-        mv.getModel().put("error", error);
+        mv.getModel().put("successmessage", success);
+        mv.getModel().put("errormessage", error);
+        mv.getModel().put("inputerrormessage", inputErrorMessage);
 
         mv.addObject("user", getCurrentUser());
 
@@ -150,12 +150,12 @@ public class AdminController {
 
         if (name.isEmpty() || address.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty() || cpassword.isEmpty()) {
             System.out.println("Sorry, fields cannot be left blank.");
-            return addOrganization(false, "Sorry, fields cannot be left blank.");
+            return addOrganization(false, "The organization could not be created. Required fields cannot be left blank.", "Sorry, required fields cannot be left blank.");
         }
 
         if (!password.equals(cpassword)) {
             System.out.println("Sorry, these passwords do not match.");
-            return addOrganization(false, "Sorry, passwords do not match.");
+            return addOrganization(false, "The organization could not be created. The passwords do not match.", "Sorry, passwords do not match.");
         }
 
 //        check if username is valid
@@ -164,13 +164,13 @@ public class AdminController {
         Matcher m = usernamePatter.matcher(username);
         if (!m.matches()) {
             System.out.println("Sorry, this username is not valid.");
-            return addOrganization(false,"Sorry, the username must be 6-30 characters, only contain letters and numbers, and start with a letter.");
+            return addOrganization(false, "The organization could not be created. The username must be 6-30 characters, only contain letters and numbers, and start with a letter.", "Sorry, the username must be 6-30 characters, only contain letters and numbers, and start with a letter.");
         }
 
 //        check if username already exists
         if (gnzUserDAO.findByName(username) != null) {
             System.out.println("Sorry, this username already exists.");
-            return addOrganization(false,"Sorry, this username already exists.");
+            return addOrganization(false, "The organization could not be created. This username already in use.","Sorry, this username is already in use.");
         }
 
 //        check if email is valid
@@ -179,7 +179,7 @@ public class AdminController {
         Matcher m2 = emailPattern.matcher(email);
         if (!m2.matches()) {
             System.out.println("Sorry, this email address is not valid.");
-            return addOrganization(false,"Sorry, this email address is invalid.");
+            return addOrganization(false, "The organization could not be created. This email address is invalid.","Sorry, this email address is invalid.");
         }
 
 //        check if url string is valid
@@ -190,7 +190,7 @@ public class AdminController {
             } catch (URISyntaxException e) {
                 e.printStackTrace();
                 System.out.println("Sorry, this site URI is not valid.");
-                return addOrganization(false, "Sorry, this site URI is invalid.");
+                return addOrganization(false, "The organization could not be created. This site URI is invalid.", "Sorry, this site URI is invalid.");
             }
         }
 
@@ -200,7 +200,7 @@ public class AdminController {
         Matcher m3 = passwordPattern.matcher(password);
         if (!m3.matches()) {
             System.out.println("Sorry, this password is not valid.");
-            return addOrganization(false,"Sorry, the password must be at least 8 characters, contain at least one letter and one number.");
+            return addOrganization(false, "The organization could not be created. The password must be at least 8 characters, contain at least one letter and one number.", "Sorry, the password must be at least 8 characters, contain at least one letter and one number.");
         }
 
         System.out.println("Creating organization...");
@@ -213,16 +213,21 @@ public class AdminController {
 
             GNZUser user = new GNZUser(username, email, Encrypter.encrytedPassword(password), organization.getId(), ADMIN.getId());
             if (this.gnzUserDAO.save(user)) {
-                return addOrganization(true, null);
+                return addOrganization(true, null, null);
             } else {
                 System.out.println("Account creation failed. Deleting organization from database...");
 //                TODO: REMOVE ORGANIZATION FROM DATABASE
+                if (this.gnzOrganizationDAO.delete(organization)) {
+                    System.out.println("Organization deleted successfully.");
+                } else {
+                    System.out.println("Organization deletion failed.");
+                }
 
-                return addOrganization(false, "Sorry, there was an error creating the account.");
+                return addOrganization(false, "Sorry, there was an error creating the account.", null);
             }
         } else {
             System.out.println("Organization creation failed.");
-            return addOrganization(false,"Sorry, there was an error creating the organization.");
+            return addOrganization(false,"Sorry, there was an error creating the organization.", null);
         }
     }
 
