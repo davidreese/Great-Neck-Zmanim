9dc9fe30e104f4ace50ac2597e69787d1286777a
David Reese <david@reesedevelopment.com>
2022-06-26 18:15:58 -0400

    enabled start-from-tab option on minyan schedule page

--- src/main/resources/static/admin/minyan-schedule.js
+++ src/main/resources/static/admin/minyan-schedule.js
@@ -15,3 +15,25 @@ $(function () {
 $('.popover-dismiss').popover({
   trigger: 'focus'
 })
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const start = urlParams.get('st');
+if (start == "sh") {
+    Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    document.getElementById("pills-shacharit-tab").classList.add("active");
+} else if (start == "mi") {
+    Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
+    document.getElementById("pills-mincha-tab").classList.add("active");
+    document.getElementById("pills-mincha").classList.add("active", "show");
+} else if (start == "ar") {
+Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    document.getElementById("pills-arvit-tab").classList.add("active");
+} else if (start == "se") {
+Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    document.getElementById("pills-selichot-tab").classList.add("active");
+} else if (start == "mr") {
+Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    document.getElementById("pills-megila-tab").classList.add("active");
+}
