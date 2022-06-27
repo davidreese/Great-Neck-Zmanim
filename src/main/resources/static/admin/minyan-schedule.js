034a7cb964cdfb380f056cc1c9cfa1186b4a7917
David Reese <david@reesedevelopment.com>
2022-06-27 10:19:28 -0400

    universalized the start-from-tab option on minyan-schedule.html

--- src/main/resources/static/admin/minyan-schedule.js
+++ src/main/resources/static/admin/minyan-schedule.js
@@ -21,19 +21,27 @@ const urlParams = new URLSearchParams(queryString);
 const start = urlParams.get('st');
 if (start == "sh") {
     Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
     document.getElementById("pills-shacharit-tab").classList.add("active");
+    document.getElementById("pills-shacharit").classList.add("active", "show");
 } else if (start == "mi") {
     Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
     Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
     document.getElementById("pills-mincha-tab").classList.add("active");
     document.getElementById("pills-mincha").classList.add("active", "show");
 } else if (start == "ar") {
-Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
     document.getElementById("pills-arvit-tab").classList.add("active");
+    document.getElementById("pills-arvit").classList.add("active", "show");
 } else if (start == "se") {
-Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
     document.getElementById("pills-selichot-tab").classList.add("active");
+    document.getElementById("pills-selichot").classList.add("active", "show");
 } else if (start == "mr") {
-Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("nav-link"), e => e.classList.remove("active"));
+    Array.prototype.forEach.call(document.getElementsByClassName("tab-pane"), e => e.classList.remove("active","show"));
     document.getElementById("pills-megila-tab").classList.add("active");
+    document.getElementById("pills-megila").classList.add("active", "show");
 }
