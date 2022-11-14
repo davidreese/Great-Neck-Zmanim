eab58bfd22adc1e3b66fc90fe2ae73bf683844de
Jacob Rosenfeld <me@jacobrosenfeld.me>
2022-11-14 00:51:12 -0500

    termination for home page?

--- src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
@@ -126,11 +126,10 @@ public class ZmanimController {
             LocalDate ref = LocalDate.of(date.getYear() + 1900, date.getMonth(), date.getDate()).plusMonths(1);
             Date startDate = minyan.getStartDate(ref);
             Date now = new Date();
-            //Date terminationDate = new Date(now.getTime() - (60000 * 8));
+            Date terminationDate = new Date(now.getTime() - (60000 * 8));
             System.out.println("SD: " + startDate);
-            //System.out.println("TD: " + terminationDate);
-            //if (startDate != null && (startDate.after(terminationDate) || now.getDate() != startDate.getDate())) {
-            if (startDate != null || now.getDate() != startDate.getDate()) {    
+            System.out.println("TD: " + terminationDate);
+            if (startDate != null && (startDate.after(terminationDate) || now.getDate() != startDate.getDate())) {  
                 String organizationName;
                 Nusach organizationNusach;
                 String organizationId;
