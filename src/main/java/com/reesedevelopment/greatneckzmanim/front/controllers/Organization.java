be968cf123c1d59078e973992949b7f5b2e77927
Jacob Rosenfeld <me@jacobrosenfeld.me>
2022-11-14 17:27:41 -0500

    Create Organization.java

---
+++ src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/Organization.java
@@ -0,0 +1,56 @@
+package com.reesedevelopment.greatneckzmanim.front.controllers;
+
+import com.reesedevelopment.greatneckzmanim.admin.structure.GNZObject;
+import com.reesedevelopment.greatneckzmanim.admin.structure.IDGenerator;
+import com.reesedevelopment.greatneckzmanim.global.Nusach;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.net.URI;
+
+@Table(name="ORGANIZATION")
+public class Organization extends GNZObject implements IDGenerator {
+    @Column(name="NAME", nullable = false)
+    private String name;
+
+    @Column(name="ADDRESS", nullable = true)
+    private String address;
+
+    @Column(name="SITE_URI", nullable = true)
+    private URI websiteURI;
+
+    @Column(name="NUSACH", nullable = false)
+    private Nusach nusach;
+
+    public Organization(String id, String username, String address, URI websiteURI, Nusach nusach) {
+        super.id = id;
+        this.name = username;
+        this.address = address;
+        this.websiteURI = websiteURI;
+        this.nusach = nusach;
+    }
+
+    public Organization(String username, String address, URI websiteURI, Nusach nusach) {
+        super.id = generateID('O');
+        this.name = username;
+        this.address = address;
+        this.websiteURI = websiteURI;
+        this.nusach = nusach;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public URI getWebsiteURI() {
+        return websiteURI;
+    }
+
+    public Nusach getNusach() {
+        return nusach;
+    }
+}
