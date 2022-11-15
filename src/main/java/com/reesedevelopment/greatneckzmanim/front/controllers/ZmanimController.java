bf809a6ffde59f7a38ee8e7e7e32c5145b6d2463
Jacob Rosenfeld <me@jacobrosenfeld.me>
2022-11-14 20:53:14 -0500

    Update ZmanimController.java

--- src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
@@ -138,7 +138,7 @@ public class ZmanimController {
             Date terminationDate = new Date(now.getTime() + (60000 * 8));
             System.out.println("SD: " + startDate);
             System.out.println("TD: " + terminationDate);
-            if (startDate != null && (startDate.after(terminationDate) || now.getDate() != startDate.getDate())) {  
+            if (startDate != null && (startDate.after(terminationDate) || now.getDate() < startDate.getDate())) {  
                 String organizationName;
                 Nusach organizationNusach;
                 String organizationId;
