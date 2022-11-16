61c03bd531625236b13b93e3f4941e06a249b43c
Jacob Rosenfeld <me@jacobrosenfeld.me>
2022-11-16 12:31:49 -0500

    updated new-minyan from origin - changed to maariv

--- src/main/resources/static/admin/new-minyan.js
+++ src/main/resources/static/admin/new-minyan.js
@@ -2,15 +2,15 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const start = urlParams.get('st');
 if (start == "sh") {
-    document.getElementById("type").value = "shacharit";
+    updateMinyanType('shacharit');
 } else if (start == "mi") {
-    document.getElementById("type").value = "mincha";
+    updateMinyanType('mincha');
 } else if (start == "ar") {
-    document.getElementById("type").value = "maariv";
+    updateMinyanType('maariv');
 } else if (start == "se") {
-    document.getElementById("type").value = "selichot";
+    updateMinyanType('selichot');
 } else if (start == "mr") {
-    document.getElementById("type").value = "megilareading";
+    updateMinyanType('megilareading');
 }
 
 function update(name) {
@@ -99,3 +99,120 @@ function updateAll() {
 //    } else {
 //    return true;
 //}
+
+
+function applyMondayThroughFriday() {
+    var mondayMode = document.getElementById(`monday-time-type`).value;
+
+    const needToMatch =  ["tuesday", "wednesday", "thursday", "friday"];
+
+    needToMatch.forEach(setMatching);
+
+    function setMatching(name) {
+        updateMode(name, mondayMode);
+        if (mondayMode == "nm") {
+
+        } else if (mondayMode == "dynamic") {
+            var mondayZman = document.getElementById(`monday-zman`).value;
+            var mondayOffset = document.getElementById(`monday-zman-offset`).value;
+            updateDynamicTime(name, mondayZman, mondayOffset);
+        } else if (mondayMode == "fixed") {
+            var mondayTime = document.getElementById(`monday-fixed-time`).value;
+            updateFixedTime(name, mondayTime);
+        }
+    }
+
+}
+
+function updateMode(name, mode) {
+    document.getElementById(`${name}-time-type`).value = mode;
+    update(name);
+}
+
+function updateDynamicTime(name, zman, offset) {
+    document.getElementById(`${name}-zman`).value = zman;
+    document.getElementById(`${name}-zman-offset`).value = offset;
+}
+
+function updateFixedTime(name, time) {
+    document.getElementById(`${name}-fixed-time`).value = time;
+}
+
+/**
+* Updates the relevant fields to match the attributes of BRANCH_MINYAN
+*/
+function branch() {
+    console.log("Branching...")
+    // console.log(BRANCH_MINYAN);
+
+    updateMinyanType(BRANCH_MINYAN.type.toLowerCase())
+    updateLocation(BRANCH_MINYAN.location.id)
+
+    updateTime('sunday', BRANCH_MINYAN.schedule.sunday);
+    updateTime('monday', BRANCH_MINYAN.schedule.monday);
+    updateTime('tuesday', BRANCH_MINYAN.schedule.tuesday);
+    updateTime('wednesday', BRANCH_MINYAN.schedule.wednesday);
+    updateTime('thursday', BRANCH_MINYAN.schedule.thursday);
+    updateTime('friday', BRANCH_MINYAN.schedule.friday);
+    updateTime('shabbat', BRANCH_MINYAN.schedule.shabbat);
+    updateTime('yt', BRANCH_MINYAN.schedule.yomTov);
+    updateTime('rc', BRANCH_MINYAN.schedule.roshChodesh);
+    updateTime('chanuka', BRANCH_MINYAN.schedule.chanuka);
+    updateTime('rcc', BRANCH_MINYAN.schedule.roshChodeshChanuka);
+
+    updateNusach(BRANCH_MINYAN.nusachString.toLowerCase());
+}
+
+function getModeFromMinyanTime(minyanTime) {
+    if (minyanTime.dynamic) {
+        return 'dynamic';
+    } else if (minyanTime.fixed) {
+        return 'fixed';
+    } else {
+        return 'nm';
+    }
+}
+
+function updateTime(name, minyanTime) {
+    var mode = getModeFromMinyanTime(minyanTime);
+    updateMode(name, mode);
+
+    if (minyanTime.dynamic) {
+        updateDynamicTime(name, minyanTime.rule.zman.toLowerCase(), minyanTime.rule.offsetMinutes);
+    } else if (minyanTime.fixed) {
+        updateFixedTime(name, minyanTime.fixedTimeShort);
+    }
+}
+
+/**
+* Updates minyan type selector
+* @param {string} type Either 'shacharit'/'mincha'/'maariv'/'selichot'/'megilareading'
+*/
+function updateMinyanType(type) {
+    updateSelector('type', type);
+}
+
+/**
+* Updates minyan location selector
+* @param {string} locId The location ID to update to
+*/
+function updateLocation(locId) {
+    updateSelector('location', locId);
+}
+
+/**
+* Updates minyan nusach selector
+* @param {string} locId The location ID to update to
+*/
+function updateNusach(nusach) {
+    updateSelector('nusach', nusach);
+}
+
+/**
+* Updates selectors
+* @param {string} id ID of the selector to update
+* @param {string} value What to update the selector to
+*/
+function updateSelector(id, value) {
+    document.getElementById(id).value = value;
+}
