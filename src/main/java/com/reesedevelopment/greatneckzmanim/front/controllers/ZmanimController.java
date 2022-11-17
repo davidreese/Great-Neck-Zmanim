43535793e28834b988c8a93eddf1c9436fe0243d
Jacob Rosenfeld <me@jacobrosenfeld.me>
2022-11-17 10:35:49 -0500

    fix kol

--- src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/front/controllers/ZmanimController.java
@@ -9,6 +9,7 @@ import com.reesedevelopment.greatneckzmanim.admin.structure.minyan.MinyanDAO;
 import com.reesedevelopment.greatneckzmanim.admin.structure.organization.Organization;
 import com.reesedevelopment.greatneckzmanim.admin.structure.organization.OrganizationDAO;
 import com.reesedevelopment.greatneckzmanim.front.MinyanEvent;
+import com.reesedevelopment.greatneckzmanim.front.KolhaMinyanim;
 import com.reesedevelopment.greatneckzmanim.global.Nusach;
 import com.reesedevelopment.greatneckzmanim.global.Zman;
 import com.reesedevelopment.greatneckzmanim.front.ZmanimHandler;
@@ -138,7 +139,8 @@ public class ZmanimController {
             Date terminationDate = new Date(now.getTime() - (60000 * 8));
             System.out.println("SD: " + startDate);
             System.out.println("TD: " + terminationDate);
-            if (startDate != null && (startDate.after(terminationDate) || now.getDate() != startDate.getDate())) {  
+            // if (startDate != null && (startDate.after(terminationDate) || now.getDate() != startDate.getDate())) {  
+            if (startDate != null && (startDate.after(terminationDate))) {      
                 String organizationName;
                 Nusach organizationNusach;
                 String organizationId;
@@ -179,25 +181,81 @@ public class ZmanimController {
                 }
             }*/
         }
+// KolhaMinyanim insertion
+List<KolhaMinyanim> kolhaMinyanims = new ArrayList<>();
 
+for (Minyan minyan : enabledMinyanim) {
+    LocalDate ref = LocalDate.of(date.getYear() + 1900, date.getMonth(), date.getDate()).plusMonths(1);
+    Date startDate = minyan.getStartDate(ref);
+    Date now = new Date();
+    System.out.println("SD: " + startDate);
+    if (startDate != null) {      
+        String organizationName;
+        Nusach organizationNusach;
+        String organizationId;
+        Organization organization = minyan.getOrganization();
+        if (organization == null) {
+            Organization temp = organizationDAO.findById(minyan.getOrganizationId());
+            organizationName = temp.getName();
+            organizationNusach = temp.getNusach();
+            organizationId = temp.getId();
+        } else {
+            organizationName = organization.getName();
+            organizationNusach = organization.getNusach();
+            organizationId = organization.getId();
+        }
+
+        String locationName = null;
+        Location location = minyan.getLocation();
+        if (location == null) {
+            location = locationDAO.findById(minyan.getLocationId());
+            if (location != null) {
+                locationName = location.getName();
+            }
+        } else {
+            locationName = location.getName();
+        }
+
+        String dynamicDisplayName = minyan.getMinyanTime().dynamicDisplayName();
+        if (dynamicDisplayName != null) {
+            kolhaMinyanims.add(new KolhaMinyanim(minyan.getId(), minyan.getType(), organizationName, organizationNusach, organizationId, locationName, startDate, dynamicDisplayName, minyan.getNusach(), minyan.getNotes()));
+        } else {
+            kolhaMinyanims.add(new KolhaMinyanim(minyan.getId(), minyan.getType(), organizationName, organizationNusach, organizationId, locationName, startDate, minyan.getNusach(), minyan.getNotes()));
+        }
+    } /*else {
+        if (startDate != null) {
+            System.out.println("Skipping minyan with start date: " + startDate.toString());
+        } else {
+            System.out.println("Skipping minyan with null start date.");
+        }
+    }*/
+}
+kolhaMinyanims.sort(Comparator.comparing(KolhaMinyanim::getStartTime));
+mv.getModel().put("kolminyanim", kolhaMinyanims);
+//end kol
+//orgs
+List<Organization> shulNames = new ArrayList<>();
+for (Organization organization : shulNames)
+mv.getModel().put("shuls", shulNames);
+//end orgs
         minyanEvents.sort(Comparator.comparing(MinyanEvent::getStartTime));
         mv.getModel().put("allminyanim", minyanEvents);
 
         List<MinyanEvent> shacharitMinyanim = new ArrayList<>();
         List<MinyanEvent> minchaMinyanim = new ArrayList<>();
-        List<MinyanEvent> arvitMinyanim = new ArrayList<>();
+        List<MinyanEvent> maarivMinyanim = new ArrayList<>();
         for (MinyanEvent me : minyanEvents) {
             if (me.getType().isShacharit()) {
                 shacharitMinyanim.add(me);
             } else if (me.getType().isMincha()) {
                 minchaMinyanim.add(me);
             } else if (me.getType().isMaariv()) {
-                arvitMinyanim.add(me);
+                maarivMinyanim.add(me);
             }
         }
         mv.getModel().put("shacharitMinyanim", shacharitMinyanim);
         mv.getModel().put("minchaMinyanim", minchaMinyanim);
-        mv.getModel().put("arvitMinyanim", arvitMinyanim);
+        mv.getModel().put("maarivMinyanim", maarivMinyanim);
 
         return mv;
     }
@@ -319,25 +377,24 @@ public class ZmanimController {
                 }
             }
         }
-
         minyanEvents.sort(Comparator.comparing(MinyanEvent::getStartTime));
         mv.getModel().put("allminyanim", minyanEvents);
 
         List<MinyanEvent> shacharitMinyanim = new ArrayList<>();
         List<MinyanEvent> minchaMinyanim = new ArrayList<>();
-        List<MinyanEvent> arvitMinyanim = new ArrayList<>();
+        List<MinyanEvent> maarivMinyanim = new ArrayList<>();
         for (MinyanEvent me : minyanEvents) {
             if (me.getType().isShacharit()) {
                 shacharitMinyanim.add(me);
             } else if (me.getType().isMincha()) {
                 minchaMinyanim.add(me);
             } else if (me.getType().isMaariv()) {
-                arvitMinyanim.add(me);
+                maarivMinyanim.add(me);
             }
         }
         mv.getModel().put("shacharitMinyanim", shacharitMinyanim);
         mv.getModel().put("minchaMinyanim", minchaMinyanim);
-        mv.getModel().put("arvitMinyanim", arvitMinyanim);
+        mv.getModel().put("maarivMinyanim", maarivMinyanim);
 
 //        mv.getModel().put("usesLocations", minyanEvents.)
 
