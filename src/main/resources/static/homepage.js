e7acd9926b290a43c0a760378e1c8d33301247f5
David Reese <david@reesedevelopment.com>
2023-03-15 16:11:51 -0400

    saturday replacement

---
+++ src/main/resources/static/homepage.js
@@ -0,0 +1,23 @@
+function replaceSaturdayWithShabbat() {
+// Get the root node of the document
+const root = document.documentElement;
+
+// Create a tree walker to get all text nodes
+const treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false);
+
+// Loop through all text nodes and replace the text
+let node;
+while (node = treeWalker.nextNode()) {
+  const text = node.textContent;
+  
+  // Replace 'Saturday' with 'Shabbat' in the text content
+  const replacedText = text.replace(/\bSaturday\b/g, 'Shabbat');
+  
+  // If the text content was modified, update the node
+  if (text !== replacedText) {
+    node.textContent = replacedText;
+  }
+}
+}
+
+replaceSaturdayWithShabbat();
