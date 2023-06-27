e44f0ff35fad6ad2d4ff17f75303dee1b818039e
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-06-27 11:56:40 -0400

    remove minus

--- src/main/java/com/reesedevelopment/greatneckzmanim/admin/structure/minyan/Minyan.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/admin/structure/minyan/Minyan.java
@@ -333,7 +333,7 @@ public class Minyan extends GNZObject implements IDGenerator {
         if (t == null) {
             return null;
         }
-        LocalDate temp = date.minusMonths(1).minusYears(1900);
+        LocalDate temp = date;
         //  return new
         return new Date(temp.getYear(), temp.getMonthValue(), temp.getDayOfMonth(), t.getHours(), t.getMinutes(), t.getSeconds());
     }
--- src/main/java/com/reesedevelopment/greatneckzmanim/admin/structure/minyan/TimeRule.java
+++ src/main/java/com/reesedevelopment/greatneckzmanim/admin/structure/minyan/TimeRule.java
@@ -26,7 +26,7 @@ class TimeRule {
 
     public Time getTime(LocalDate date) {
         ZmanimHandler zmanimHandler = new ZmanimHandler();
-        LocalDate temp = date.minusMonths(1);
+        LocalDate temp = date;
         Date zmanTime = zmanimHandler.getZmanim(temp).get(zman);
 //        TODO: DEAL WITH DEPRECATED FUNCTIONS
         Time t = new Time(zmanTime.getHours(), zmanTime.getMinutes() + offsetMinutes, zmanTime.getSeconds(), 0);
